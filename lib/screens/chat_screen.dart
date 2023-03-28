ae82bffdee83d7f94d6a1918ff6d10fdd4d926ce
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-28 11:55:22 +0100

    creating chatscreen ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,9 @@
+import 'package:ask_anything/constant/constants.dart';
 import 'package:ask_anything/services/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -11,20 +13,82 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  bool _isTyping = false;
+
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("ChatGPT"),
-        elevation: 2,
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset(AssetManager.openaiLogo),
+        appBar: AppBar(
+          title: const Text("ChatGPT"),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert_rounded,
+                  color: Colors.white,
+                ))
+          ],
+          elevation: 2,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(AssetManager.openaiLogo),
+          ),
         ),
-      ),
-      body: Center(
-        child: Image.asset(AssetManager.openaiLogo),
-      ),
-    );
+        body: SafeArea(
+            child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return const Text("Hello this is a text");
+                  }),
+            ),
+            if (_isTyping) ...[
+              const SpinKitThreeBounce(
+                color: Colors.white,
+                size: 18,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Material(
+                color: cardColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        controller: textEditingController,
+                        onSubmitted: (value) {
+                          // send message
+                        },
+                        decoration: InputDecoration.collapsed(
+                            hintText: "How can I help you",
+                            hintStyle: TextStyle(color: Colors.grey)),
+                      )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          ))
+                    ],
+                  ),
+                ),
+              )
+            ]
+          ],
+        )));
   }
 }
